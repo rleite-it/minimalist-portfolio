e2a1a62bffeac2051c31589a0384fe0334f15240
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-13 00:13:41 +0100

    update app file

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Work from "./pages/work"
 import About from "./pages/about"
+import AppLayout from "./layouts/app-layout"
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Work />
-  },
-  {
-    path: "/about",
-    element: <About />
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Work />
+      },
+      {
+        path: "/about",
+        element: <About />
+      }
+    ]
   }
-])
+]);
 
 function App() {
-  return <main>
+  return <main className="h-screen flex flex-col items-center px-4">
     <RouterProvider router={router} />
   </main>
 }
 
-export default App
+export default App;
