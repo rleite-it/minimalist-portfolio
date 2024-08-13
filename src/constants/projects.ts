688e73fad725ba0c189e049195da3b4efca90178
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-13 18:43:54 +0100

    remove reduntant projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -18,24 +18,4 @@ export const PROJECTS: ProjectProps[] = [
 		preview: "./src/assets/projects-preview/ama/create-room.png",
 		wip: false,
 	},
-	{
-		id: "01",
-		name: "ASK ME ANYTHING",
-		description:
-			"Ask Me Anything app with real-time updates and modern React features. Learned key techniques in WebSocket integration and state management.",
-		stack:
-			"Developed with Go and React, styled with TailwindCSS, containerized using Docker.",
-		preview: "./src/assets/projects-preview/ama/create-room.png",
-		wip: false,
-	},
-	{
-		id: "01",
-		name: "ASK ME ANYTHING",
-		description:
-			"Ask Me Anything app with real-time updates and modern React features. Learned key techniques in WebSocket integration and state management.",
-		stack:
-			"Developed with Go and React, styled with TailwindCSS, containerized using Docker.",
-		preview: "./src/assets/projects-preview/ama/create-room.png",
-		wip: false,
-	},
 ];
