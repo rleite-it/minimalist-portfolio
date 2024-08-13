9f4badd7572815e01b31040ee95c03ee488f7fff
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-13 18:22:20 +0100

    add work page component

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,9 +1,11 @@
+import Projects from '../components/projects'
 import WorkIntroduction from '../components/work-introduction'
 
 export default function Work() {
     return (
         <div className='w-[1024px] mt-32'>
             <WorkIntroduction />
+            <Projects />
         </div>
     )
 }
