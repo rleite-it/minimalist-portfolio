46cf9ae3579e8b9b75d792ed1e64a297bac5a413
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-14 00:35:12 +0100

    update navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import NavItem from './nav-item'
 
 const NAV_ITEMS: Array<{
@@ -29,7 +30,7 @@ export default function Navbar() {
 
     return (
         <nav className='py-12 max-w-[1024px] w-full flex justify-between items-baseline'>
-            <span className='text-4xl'>rl.</span>
+            <span className='text-4xl cursor-pointer'><Link to="/">rl.</Link></span>
 
             <ul className='flex gap-16'>
                 {NAV_ITEMS.map((item) => <NavItem key={item.key} item={item.value} path={item.path} />)}
