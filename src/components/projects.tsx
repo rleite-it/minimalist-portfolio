f154f96129d74ca130e93e94114a79613526f55e
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-13 18:21:58 +0100

    add projects list component

---
+++ src/components/projects.tsx
@@ -0,0 +1,16 @@
+import Project from "./project";
+
+import { PROJECTS } from "../constants/projects";
+
+export default function Projects() {
+    return (
+        <div className="w-full flex flex-col pt-8">
+            {PROJECTS.map((project, i) => (
+                <>
+                    {i !== 0 ? <div className="mt-20 h-[.25px] bg-[#181717]" /> : <></>}
+                    <Project key={`project-${project.id}`} id={project.id} name={project.name} description={project.description} stack={project.stack} preview={project.preview} wip={project.wip} />
+                </>
+            ))}
+        </div >
+    )
+}
