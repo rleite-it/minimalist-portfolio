0b385ed3eb0c3ffbf1eadf90a645277a1780ea77
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-13 18:45:31 +0100

    remove index from map method

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,7 +5,7 @@ import { PROJECTS } from "../constants/projects";
 export default function Projects() {
     return (
         <div className="w-full flex flex-col pt-8">
-            {PROJECTS.map((project, i) => (
+            {PROJECTS.map((project) => (
                 <>
                     <Project key={`project-${project.id}`} id={project.id} name={project.name} description={project.description} stack={project.stack} preview={project.preview} wip={project.wip} />
                     <div className="mt-20 h-[.25px] bg-[#181717]" />
