cfea9b0d84cfe7ca375fd8cab56689e20814268f
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-13 18:44:42 +0100

    update projects list component logic for the divider

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,8 +7,8 @@ export default function Projects() {
         <div className="w-full flex flex-col pt-8">
             {PROJECTS.map((project, i) => (
                 <>
-                    {i !== 0 ? <div className="mt-20 h-[.25px] bg-[#181717]" /> : <></>}
                     <Project key={`project-${project.id}`} id={project.id} name={project.name} description={project.description} stack={project.stack} preview={project.preview} wip={project.wip} />
+                    <div className="mt-20 h-[.25px] bg-[#181717]" />
                 </>
             ))}
         </div >
