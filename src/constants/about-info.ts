dc3179d1c299e4caf2f567ac58e9e7899a3d9575
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-14 14:04:50 +0100

    add about info file with skills and software

---
+++ src/constants/about-info.ts
@@ -0,0 +1,13 @@
+export const SOFTWARE: string[] = ["VS Code", "Neovim"];
+
+export const SKILLS: string[] = [
+	"React",
+	"Javascript",
+	"Typescript",
+	"HTML",
+	"CSS",
+	"NodeJS",
+	"Express",
+	"PostgreSQL",
+	"MongoDB",
+];
