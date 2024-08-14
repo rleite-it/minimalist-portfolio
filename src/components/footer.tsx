27526f8fb37b9f958165b5f75a4112e75dc4acbe
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-14 23:23:36 +0100

    update footer link to open in a new tab

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer className="mt-28 static py-11 w-[1024px] min-h-16 flex items-center justify-between text-[15px]">
-            <span className="text-zinc-500">Design credits - <Link to="https://www.figma.com/@feyza" className="underline transition-colors hover:text-zinc-950">Feyza Okumus</Link></span>
+            <span className="text-zinc-500">Design credits - <Link to="https://www.figma.com/@feyza" className="underline transition-colors hover:text-zinc-950" target="_blank">Feyza Okumus</Link></span>
             <ul className="flex gap-16 font-semibold">
                 <li>
                     <Link target="_blank" to="https://github.com/rleite-it">
