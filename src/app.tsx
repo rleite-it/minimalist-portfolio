10ce05b158a26272d6daa4308ef09185eeaa6841
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-14 21:47:04 +0100

    move cursor component out of the app layout to the app root

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,8 @@ import About from "./pages/about"
 import AppLayout from "./layouts/app-layout"
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import Error from "./pages/error";
+import Cursor from "./components/cursor";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +21,8 @@ const router = createBrowserRouter([
         path: "/about",
         element: <About />
       }
-    ]
+    ],
+    errorElement: <Error />
   }
 ]);
 
@@ -27,6 +30,7 @@ function App() {
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
       <main className="h-screen flex flex-col items-center px-4">
+        <Cursor />
         <RouterProvider router={router} />
       </main>
     </Worker>
--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -2,12 +2,10 @@ import { Suspense } from 'react'
 import Navbar from '../components/navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/footer'
-import Cursor from '../components/cursor'
 
 export default function AppLayout() {
     return (
         <>
-            <Cursor />
             <Navbar />
             <Suspense fallback={<h1>Loading...</h1>}>
                 <Outlet />
