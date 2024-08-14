a9b418d40deda834ba441ca42b9afecf6568639c
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-14 18:12:25 +0100

    add missing worker for pdf viewer library

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
+import { Worker } from "@react-pdf-viewer/core";
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import Work from "./pages/work"
 import About from "./pages/about"
 import AppLayout from "./layouts/app-layout"
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const router = createBrowserRouter([
   {
@@ -20,9 +24,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <main className="h-screen flex flex-col items-center px-4">
-    <RouterProvider router={router} />
-  </main>
+  return (
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <main className="h-screen flex flex-col items-center px-4">
+        <RouterProvider router={router} />
+      </main>
+    </Worker>
+  )
 }
 
 export default App;
