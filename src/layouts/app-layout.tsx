e7748533bc30323b65c11c9cc6abe939effeaa3b
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-15 00:19:07 +0100

    add loading page with custom animation

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -2,12 +2,13 @@ import { Suspense } from 'react'
 import Navbar from '../components/navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/footer'
+import Loading from '../pages/loading'
 
 export default function AppLayout() {
     return (
         <>
             <Navbar />
-            <Suspense fallback={<h1>Loading...</h1>}>
+            <Suspense fallback={<Loading />}>
                 <Outlet />
             </Suspense>
             <Footer />
---
+++ src/pages/loading.tsx
@@ -0,0 +1,116 @@
+export default function Loading() {
+    const m = 0.512286623256592433;
+
+    function buildWave(w: number, h: number) {
+        const a = h / 4;
+        const y = h / 2;
+
+        const pathData = [
+            "M",
+            w * 0,
+            y + a / 2,
+            "c",
+            a * m,
+            0,
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+            "s",
+            -(1 - a) * m,
+            a,
+            a,
+            a,
+            "s",
+            -(1 - a) * m,
+            -a,
+            a,
+            -a,
+        ].join(" ");
+
+        return pathData;
+    }
+
+
+    return (
+        <div className="w-full h-full flex">
+            <div className="flex items-center border-4 border-zinc-950 h-28 w-28 m-auto">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    width="80px" height="60px"
+                    viewBox="5 0 80 60"
+                    className="m-auto overflow-hidden"
+                >
+                    <path id="wave"
+                        fill="none"
+                        stroke="#262626"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                        strokeDasharray="0 16 101 16"
+                        d={buildWave(90, 60)}>
+                    </path>
+                </svg>
+            </div>
+        </div>
+    )
+}
