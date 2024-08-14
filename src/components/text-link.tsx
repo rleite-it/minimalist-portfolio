89e9c8c0d2085a10497b6efada8a51253c9821be
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-14 14:04:14 +0100

    update text link component

--- src/components/text-link.tsx
+++ src/components/text-link.tsx
@@ -1,13 +1,17 @@
-import { ReactElement } from "react";
+import { HTMLAttributes, ReactElement } from "react";
 
-interface TextLinkProps {
+interface TextLinkProps extends HTMLAttributes<HTMLSpanElement> {
     text: string;
     icon?: ReactElement;
     wip?: boolean
 }
 
-export default function TextLink({ text, icon, wip = false }: TextLinkProps) {
+export default function TextLink({ text, icon, wip = false, className = "", ...rest }: TextLinkProps) {
     return (
-        <span data-wip={wip} className="flex items-start underline cursor-pointer font-medium select-none data-[wip=true]:text-zinc-500">{text} {icon}</span>
+        <span
+            data-wip={wip}
+            className={`flex items-start underline cursor-pointer font-medium select-none data-[wip=true]:text-zinc-500 ${className}`}
+            {...rest} // Spread the rest of the props to the span element
+        >{text} {icon}</span>
     )
 }
