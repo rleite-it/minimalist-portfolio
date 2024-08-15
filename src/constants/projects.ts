9f06a85d732e89f9bd26e4c58d3424a9d4abe37a
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-15 17:56:50 +0100

    update project status on projects constant file

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -16,7 +16,7 @@ export const PROJECTS: ProjectProps[] = [
 			"Ask Me Anything app with real-time updates and modern React features. Learned key techniques in WebSocket integration and state management.",
 		stack:
 			"Developed with Go and React, styled with TailwindCSS, containerized using Docker.",
-		preview: "./src/assets/projects-preview/ama/create-room.webp",
+		preview: "./projects-preview/ama/create-room.webp",
 		repository: "https://github.com/rleite-it/ask-me-anything",
 		wip: false,
 	},
@@ -26,8 +26,8 @@ export const PROJECTS: ProjectProps[] = [
 		description:
 			"Minimalist portfolio built with React. Focused on optimizing performance and seamless user experience.",
 		stack: "Developed with React and styled with TailwindCSS.",
-		preview: "./src/assets/projects-preview/minimalist-portfolio/work.webp",
+		preview: "./projects-preview/minimalist-portfolio/work.webp",
 		repository: "https://github.com/rleite-it/minimalist-portfolio",
-		wip: true,
+		wip: false,
 	},
 ];
