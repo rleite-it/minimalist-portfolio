8eb11d5e43aba56d81f044fa0c4f9cf094d9cb51
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-18 18:41:16 +0100

    update cursor component styles

--- src/components/cursor.tsx
+++ src/components/cursor.tsx
@@ -27,7 +27,6 @@ export default function Cursor() {
         <div
             ref={cursorRef}
             className="fixed top-[-30px] left-[-30px] w-16 h-16 pointer-events-none z-50 mix-blend-difference"
-            style={{ pointerEvents: 'none' }}
         >
             <svg width="100%" height="100%">
                 <circle cx="50%" cy="50%" r="25%" fill="white" />
