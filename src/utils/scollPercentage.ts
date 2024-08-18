aa8e65caf98f2d10414564a243bc4246c14bd58d
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-18 18:41:47 +0100

    add util function to get scrollbar percetage

---
+++ src/utils/scollPercentage.ts
@@ -0,0 +1,10 @@
+export default function getScrollbarPercentage(): number {
+	const h = document.documentElement;
+	const b = document.body;
+	const st = "scrollTop";
+	const sh = "scrollHeight";
+
+	return Math.floor(
+		((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100
+	);
+}
